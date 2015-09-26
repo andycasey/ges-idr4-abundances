59086d8384a110d2922f2c379446765014af01f0
Andy Casey <andycasey@gmail.com>
2015-09-26 17:53:26 +0100

    Added S 1 homogenisation script

---
+++ homogenise_s1.py
@@ -0,0 +1,41 @@
+
+""" Produce ensemble S 1 abundances from the Gaia-ESO Survey iDR4 WG11 data """
+
+__author__ = 'Andy Casey <arc@ast.cam.ac.uk>'
+
+
+import release
+
+database, element, ion = ("arc", "S", 1)
+ges = release.DataRelease(database)
+
+flag_id = ges.flags.retrieve_or_create("Abundance is non-physical")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = '{1}' AND
+        (abundance > 9)""".format(element, ion))
+
+# ULB and EPINARBO show very different results (~0.4 dex offset) for lines bluer
+# than 6757.2. Thus, we will just use the 6757.2 line.
+flag_id = ges.flags.retrieve_or_create(
+    "Not an ideal line for analysis")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = '{1}'
+    AND wavelength < 6757 AND abundance != 'NaN'""".format(element, ion))
+
+# Calculate biases and apply them.
+species_biases = ges.biases.differential(element, ion)
+for node in species_biases:
+    for wavelength, (bias, sigma, N) in species_biases[node].items():
+        rows = ges.biases.apply_offset(element, ion, node, wavelength, -bias)
+
+# Perform the homogenisation.
+ges.homogenise.species(element, ion)
+
+# Produce some figures.
+fig = ges.plot.differential_line_abundances(element, ion, scaled=True)
+fig.savefig("figures/{0}{1}/scaled-differential-line-abundances.png".format(
+    element.upper(), ion))
+
+fig = ges.plot.solar_comparison(element, ion, scaled=True)
+fig.savefig("figures/{0}{1}/scaled-compare-solar.png".format(element.upper(),
+    ion))
