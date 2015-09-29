3e887aed99cdbae62bfe3f2b8c015cb62e7266af
Andy Casey <andycasey@gmail.com>
2015-09-29 09:52:43 +0100

    Added homogenisation script for C 1

---
+++ homogenise_c1.py
@@ -0,0 +1,34 @@
+
+""" Produce ensemble C 1 abundances from the Gaia-ESO Survey iDR4 WG11 data """
+
+__author__ = 'Andy Casey <arc@ast.cam.ac.uk>'
+
+import release
+
+database, element, ion = ("arc", "C", 1)
+ges = release.DataRelease(database)
+
+# 2 lines (5052.1 and 5380.3) from 2 nodes (Lumba and CAUP)
+
+# Lumba measurements for feh < -2.5 stars are bad, but may be salvageable.
+
+# Calculate biases and apply them.
+species_biases = ges.biases.differential(element, ion)
+for node in species_biases:
+    for wavelength, (bias, sigma, N) in species_biases[node].items():
+        rows = ges.biases.apply_offset(element, ion, node, wavelength, -bias)
+
+# Perform the homogenisation.
+result = ges.homogenise.species(element, ion)
+
+# Produce some figures.
+fig = ges.plot.differential_line_abundances(element, ion, scaled=True)
+fig.savefig("figures/{0}/scaled-differential-line-abundances.png".format(_))
+
+fig = ges.plot.solar_comparison(element, ion, scaled=True)
+fig.savefig("figures/{0}/scaled-compare-solar.png".format(_))
+
+_ = "{0}{1}".format(element.upper(), ion)
+fig = ges.plot.benchmark_line_abundances(element, ion, "benchmarks.yaml",
+    scaled=True)
+fig.savefig("figures/{0}/scaled-benchmark-line-abundances.png".format(_))
