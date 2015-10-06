4f48b44a709331fbee91bb7a2a3f17aaed5c3f31
Andy Casey <andycasey@gmail.com>
2015-10-06 15:28:15 +0100

    Updated Cr 1 script:

--- homogenise_cr1.py
+++ homogenise_cr1.py
@@ -8,17 +8,126 @@ import release
 database, element, ion = ("arc", "Cr", 1)
 ges = release.DataRelease(database)
 
+flag_id = ges.flags.retrieve_or_create(
+    "Abundance is non-physical")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = {1}
+    AND (abundance < 2 OR abundance > 8)""".format(element, ion))
 
-
-
-# ULB shows very peculiar results.
+# CAUP is spurious for 5247.6 and 5348.3
 flag_id = ges.flags.retrieve_or_create(
     "Large scatter seen for this line from this node")
 num_rows = ges.flags.update([flag_id],
-    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = {1} AND
-    node like 'ULB%'""".format(element, ion))
+    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = {1}
+    AND node LIKE 'CAUP%' AND wavelength > 5247 AND wavelength < 5248""".format(
+        element, ion))
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = {1}
+    AND node LIKE 'CAUP%' AND wavelength > 5348 AND wavelength < 5349""".format(
+        element, ion))
 
-raise NotImplementedError
+# Remove EMP lines for all EPINARBO results
+flag_id = ges.flags.retrieve_or_create(
+    "Large scatter seen in this line for EMP stars")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances l JOIN (select distinct on (cname) cname,
+        feh FROM node_results) n ON (l.cname = n.cname AND 
+        l.element = '{0}' AND l.ion = {1} AND l.node LIKE 'EPINARBO%' AND
+        n.feh < -2.5)""".format(element, ion))
+
+# Remove ULB for 4789.3 
+flag_id = ges.flags.retrieve_or_create(
+    "Large scatter seen for this line from this node")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = {1}
+    AND node LIKE 'ULB%' AND wavelength > 4789 AND wavelength < 4790""".format(
+        element, ion))
+
+# Remove ULB EMP lines for 4936.3 and 5287.2 and 5304.2 and 5318.8 
+flag_id = ges.flags.retrieve_or_create(
+    "Large scatter seen in this line for EMP stars")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances l JOIN (select distinct on (cname) cname,
+        feh FROM node_results) n ON (l.cname = n.cname AND 
+        l.element = '{0}' AND l.ion = {1} AND l.node LIKE 'ULB%' AND
+        n.feh < -2.5 AND (
+            (l.wavelength > 4936 AND l.wavelength < 4937) OR
+            (l.wavelength > 5287 AND l.wavelength < 5288) OR
+            (l.wavelength > 5304 AND l.wavelength < 5305) OR
+            (l.wavelength > 5318 AND l.wavelength < 5319)
+        ))""".format(element, ion))
+
+# Remove ULB fir 5838.7 line and 5844.6
+flag_id = ges.flags.retrieve_or_create(
+    "Large scatter seen for this line from this node")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = {1}
+    AND node LIKE 'ULB%' AND (
+        (wavelength > 5838 AND wavelength < 5839) OR
+        (wavelength > 5844 AND wavelength < 5845))""".format(element, ion))
+
+# Remove LUMBA for EMP lines below 5204
+flag_id = ges.flags.retrieve_or_create(
+    "Large scatter seen in this line for EMP stars")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances l JOIN (select distinct on (cname) cname,
+        feh FROM node_results) n ON (l.cname = n.cname AND 
+        l.element = '{0}' AND l.ion = {1} AND l.node LIKE 'Lumba%' AND
+        n.feh < -2.5 AND l.wavelength < 5204)""".format(element, ion))
+
+# Remove LUMBA for EMP lines between 5214 and 5287.2 
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances l JOIN (select distinct on (cname) cname,
+        feh FROM node_results) n ON (l.cname = n.cname AND 
+        l.element = '{0}' AND l.ion = {1} AND l.node LIKE 'Lumba%' AND
+        n.feh < -2.5 AND l.wavelength > 5214 and l.wavelength < 5287)""".format(element, ion))
+
+# Remove LUMBA EMP lines from 5300 to 5345.0
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances l JOIN (select distinct on (cname) cname,
+        feh FROM node_results) n ON (l.cname = n.cname AND 
+        l.element = '{0}' AND l.ion = {1} AND l.node LIKE 'Lumba%' AND
+        n.feh < -2.5 AND l.wavelength > 5300 and l.wavelength < 5345)""".format(element, ion))
+
+# Remove LUMBA for 5480.5
+flag_id = ges.flags.retrieve_or_create(
+    "Large scatter seen for this line from this node")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = {1}
+    AND node LIKE 'Lumba%' AND (wavelength > 5480 AND wavelength < 5481)""".format(element, ion))
+
+
+# Remove LUMBA EMP for 5628.6
+flag_id = ges.flags.retrieve_or_create(
+    "Large scatter seen in this line for EMP stars")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances l JOIN (select distinct on (cname) cname,
+        feh FROM node_results) n ON (l.cname = n.cname AND 
+        l.element = '{0}' AND l.ion = {1} AND l.node LIKE 'Lumba%' AND
+        n.feh < -2.5 AND l.wavelength > 5628 AND l.wavelength < 5629)""".format(element, ion))
+
+# Remove LUMBA EMP for 5648.3 and 5702.3 
+flag_id = ges.flags.retrieve_or_create(
+    "Large scatter seen in this line for EMP stars")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances l JOIN (select distinct on (cname) cname,
+        feh FROM node_results) n ON (l.cname = n.cname AND 
+        l.element = '{0}' AND l.ion = {1} AND l.node LIKE 'Lumba%' AND
+        n.feh < -2.5 AND (
+            (l.wavelength > 5648 AND l.wavelength < 5649) OR
+            (l.wavelength > 5702 AND l.wavelength < 5703)
+        ))""".format(element, ion))
+
+# Remove LUMBA for 5838.7 and 5844.6
+flag_id = ges.flags.retrieve_or_create(
+    "Large scatter seen for this line from this node")
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = {1}
+    AND node LIKE 'Lumba%' AND (wavelength > 5838 AND wavelength < 5839)""".format(element, ion))
+num_rows = ges.flags.update([flag_id],
+    """SELECT id FROM line_abundances WHERE element = '{0}' AND ion = {1}
+    AND node LIKE 'Lumba%' AND (wavelength > 5844 AND wavelength < 5845)""".format(element, ion))
+
 
 
 # Calculate biases and apply them.
